90e5b9c71a05db940c183fef0e3164416ff88ac1
Martin Bucinskas <martin.bucinskas@enginegroup.com>
2020-01-14 16:30:02 +0000

    timezones: added a way to load and retrieve timezones from location id. Data stored to ETS.

--- apps/noel_importer/src/geonames_timezone.erl
+++ apps/noel_importer/src/geonames_timezone.erl
@@ -10,19 +10,29 @@
 -author("martin.bucinskas").
 
 %% API
--export([get_timezone_from_locid/1, get_full_entry/1]).
+-export([get_timezone_from_locid/1]).
 
 start() ->
-  ets:new(geonames_table, [geonames_table, protected, set, {keypos, 1}]).
-
-get_timezone_from_locid(LocationId) ->
-  Entry = get_full_entry(LocationId),
-  LocationId.
-
-get_full_entry(LocationId) ->
-  {ok, File} = file:read_file("cities500.txt"),
+  ets:new(geonames_table, [set, named_table]),
+  {ok, File} = file:read_file("cities500.dat"),
   Lines = [binary_to_list(Binary) || Binary <- binary:split(File, <<"\n">>, [global]), Binary =/= << >>],
   ParsedLines = lists:map(
     fun(Str) -> string:tokens(Str, "\t") end, Lines
   ),
-  lists:foreach(fun([K,V]) -> io:format("K: ~p V: ~p~n", [K, V]) end, ParsedLines).
+  loop_through_each_entry(ParsedLines).
+
+loop_through_each_entry([]) -> ok;
+loop_through_each_entry([H|T]) ->
+  store_row(H),
+  loop_through_each_entry(T).
+
+store_row([]) -> ok;
+store_row(Row) ->
+  LocationID = lists:nth(1, Row),
+  CityName = lists:nth(2, Row),
+  Timezone = lists:nth(3, Row),
+  ets:insert(geonames_table, {LocationID, CityName, Timezone}).
+
+get_timezone_from_locid(LocationId) ->
+  start(),
+  ets:lookup(geonames_table, LocationId).
